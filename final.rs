3a38f9dbb841c50854cfd7e232f696a3e01be4be
charlietambur59 <119467092+charlietambur59@users.noreply.github.com>
2022-12-20 16:53:08 -0500

    Update final.rs

--- final.rs
+++ final.rs
@@ -1,71 +1,41 @@
 fn main() {
-    // Declare a variable and initialize it
-    let mut x = 5;
-    println!("The value of x is: {}", x);
+    let x = 5;
 
-    // Mutate the value of x
-    x = 6;
-    println!("The value of x is: {}", x);
-
-    // Declare a constant
-    const MAX_POINTS: u32 = 100_000;
-    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
-
-    // Shadowing allows us to reuse a variable name with a different type
-    let x = x + 1; // x is now an i32
-    let x = x * 2; // x is now an i32
-    println!("The value of x is: {}", x);
-
-    // Tuples
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (a, b, c) = tup;
-    println!("The value of a is: {}", a);
-    println!("The value of b is: {}", b);
-    println!("The value of c is: {}", c);
-
-    // Arrays
-    let arr = [1, 2, 3, 4, 5];
-    let first = arr[0];
-    let second = arr[1];
-    println!("The value of first is: {}", first);
-    println!("The value of second is: {}", second);
-
-    // If statements
-    let condition = true;
-    let number = if condition {
-        5
-    } else {
-        6
-    };
-    println!("The value of number is: {}", number);
-
-    // Loops
-    let mut count = 0;
-    loop {
-        count += 1;
-        if count == 3 {
-            println!("Breaking loop");
-            break;
-        }
+    // Basic syntax
+    match x {
+        1 => println!("x is 1"),
+        2 => println!("x is 2"),
+        3 => println!("x is 3"),
+        _ => println!("x is something else"),
     }
 
-    // While loops
-    let mut count = 0;
-    while count <= 10 {
-        count += 1;
-        println!("The value of count is: {}", count);
+    // Matching on multiple values
+    match x {
+        1 | 2 => println!("x is 1 or 2"),
+        3 | 4 => println!("x is 3 or 4"),
+        _ => println!("x is something else"),
     }
 
-    // For loops
-    for number in (1..4).rev() {
-        println!("The value of number is: {}", number);
+    // Matching on a range of values
+    match x {
+        1...5 => println!("x is between 1 and 5"),
+        6...10 => println!("x is between 6 and 10"),
+        _ => println!("x is something else"),
     }
 
-    // Functions
-    let result = add(5, 6);
-    println!("The result of the function is: {}", result);
-}
+    // Matching on a pattern
+    let y = Some(5);
+    match y {
+        Some(n) if n > 0 => println!("y is a positive number"),
+        Some(n) if n < 0 => println!("y is a negative number"),
+        Some(_) => println!("y is zero"),
+        None => println!("y is None"),
+    }
 
-fn add(x: i32, y: i32) -> i32 {
-    x + y
+    // Matching on a type
+    let z = "hello";
+    match z {
+        "hello" => println!("z is a string"),
+        _ => println!("z is something else"),
+    }
 }
